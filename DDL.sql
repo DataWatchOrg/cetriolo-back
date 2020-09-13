b01474b3ad550d849cd6e4dd7660a7d3c507ec83
Andre Pires <andre.pires2@fatec.sp.gov.br>
2020-09-12 21:48:45 -0300

    start setup commit

---
+++ DDL.sql
@@ -0,0 +1,40 @@
+create schema cetriolo;
+ 
+use cetriolo;
+
+create user 'user'@'localhost' identified by 'fatec';
+
+grant select, insert, delete, update on cetrioloo.* to user@'localhost';
+
+create table usu_usuario (
+	id bigint unsigned primary key auto_increment,
+    usu_nome varchar(50) not null,
+    usu_email varchar(50) not null,
+    usu_telefone varchar(20)
+);
+
+create table ori_orientador (
+	ori_id bigint unsigned primary key,
+	constraint ori_usu_id foreign key (ori_id) references usu_usuario (id)
+);
+
+create table alu_aluno (
+	alu_id bigint unsigned primary key,
+	alu_curso varchar(30) not null,
+	ori_usu_id bigint unsigned,
+	constraint alu_usu_id foreign key (alu_id) references usu_usuario (id),
+	constraint ori_usu_id_fk foreign key (ori_usu_id) references ori_orientador (ori_id)
+);
+
+create table mat_materia (
+	mat_id bigint unsigned primary key auto_increment,
+	mat_nome varchar(50) not null
+);
+
+create table ama_aluno_materia (
+	alu_id bigint unsigned,
+	mat_id bigint unsigned,
+	primary key (alu_id, mat_id),
+	constraint ama_alu_fk foreign key (alu_id) references alu_aluno (alu_id),
+	constraint ama_mat_fk foreign key (mat_id) references mat_materia (mat_id)
+);
---
+++ src/test/java/dream/team/cetriolo/sprintbootapp/CetrioloApplicationTests.java
@@ -0,0 +1,13 @@
+package dream.team.cetriolo.sprintbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CetrioloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
