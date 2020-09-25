46f97684a71a32233b8a7a9537d82269d233f4c7
andre-pires2 <andre.pires2@fatec.sp.gov.br>
2020-09-25 23:19:39 +0000

    Rest start implementation

---
+++ src/main/java/dream/team/cetriolo/sprintbootapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package dream.team.cetriolo.sprintbootapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping
+    public String welcome() {
+        return "CETRIOLO UN'AUTA VOLTA!!!";
+    }
+}
